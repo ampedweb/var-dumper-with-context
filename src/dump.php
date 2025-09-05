617c5a57803ff17630e7c9e2704b6c0b171deec5
Chris Powell <chris.powell@futurenet.com>
2025-09-05 16:56:26 +0100

    Add env variables for remote/local base path and editor

--- src/dump.php
+++ src/dump.php
@@ -9,5 +9,11 @@ VarDumper::setHandler(function ($var) {
     $cloner = new VarCloner();
     $dumper = 'cli' === PHP_SAPI ? new CliDumper() : new HtmlDumper();
 
+    if ($dumper instanceof HtmlDumper) {
+        $dumper->setRemoteBasePath($_ENV('VAR_DUMPER_REMOTE_BASE_PATH') ?? null);
+        $dumper->setLocalBasePath($_ENV('VAR_DUMPER_LOCAL_BASE_PATH') ?? null);
+        $dumper->setEditor($_ENV('VAR_DUMPER_EDITOR') ?? null);
+    }
+
     $dumper->dump($cloner->cloneVar($var));
 });
